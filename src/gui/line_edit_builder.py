649fc71d23247978cb28f5704fc52d6768b757bf
sab-992 <sabri.abdi@polymtl.ca>
2026-02-22 11:45:06 -0500

    added line and text input

---
+++ src/gui/line_edit_builder.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QLineEdit
+from src.gui.detail.widget_builder import WidgetBuilder
+
+
+class LineEditBuilder(WidgetBuilder):
+    def __init__(self):
+        super().__init__(QLineEdit)
+
+    def initialize_instance(self) -> QLineEdit:
+        return self.widget_type()
---
+++ src/gui/text_edit_builder.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QTextEdit
+from src.gui.detail.widget_builder import WidgetBuilder
+
+
+class TextEditBuilder(WidgetBuilder):
+    def __init__(self):
+        super().__init__(QTextEdit)
+
+    def initialize_instance(self) -> QTextEdit:
+        return self.widget_type()
