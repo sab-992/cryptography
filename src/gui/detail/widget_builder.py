683e1d309c8e4699317f9b080da391247d4a6dad
sab-992 <sabri.abdi@polymtl.ca>
2026-02-22 13:38:08 -0500

    added possiblity to change fonts

--- src/gui/detail/widget_builder.py
+++ src/gui/detail/widget_builder.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QWidget
 from src.utils.logger import Logger, Level_en
 from typing import Self, Type, TypeVar
@@ -11,6 +12,7 @@ class WidgetBuilder(ABC):
             self.error(f"Type {cls.__name__} is not a Qt widget")
         self.width = -1
         self.height = -1
+        self.font = None
 
     def build(self) -> T:
         if self.width <= 0 or\
@@ -18,6 +20,12 @@ class WidgetBuilder(ABC):
             self.error(f"Missing dimensions ! w={self.width}, h={self.height}")
 
         widget: QWidget = self.initialize_instance()
+
+        if not self.font:
+            default_font = widget.font()
+            self.set_font(default_font.family(), default_font.pointSize())
+
+        widget.setFont(self.font)
         widget.setFixedWidth(self.width)
         widget.setFixedHeight(self.height)
 
@@ -26,6 +34,10 @@ class WidgetBuilder(ABC):
     @abstractmethod
     def initialize_instance(self) -> T:
         pass
+
+    def set_font(self, family: str, size: int, weight: int = QFont.Weight.Normal, italic: bool = False) -> Self:
+        self.font = QFont(family, size, weight, italic)
+        return self
 
     def set_height(self, height: int) -> Self:
         self.height = height
