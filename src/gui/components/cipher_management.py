a8ef62fb2c0f6069a2f420802bceee6e427ef610
sab-992 <sabri.abdi@polymtl.ca>
2026-02-25 18:19:48 -0500

    added label for cipher management buttons

--- src/gui/components/cipher_management.py
+++ src/gui/components/cipher_management.py
@@ -2,10 +2,11 @@ from pathlib import Path
 from PySide6 import QtCore, QtWidgets
 from PySide6.QtCore import Slot
 from src.cipher.detail.type import CipherDict
+from src.gui.builder.label_builder import LabelBuilder
 from src.gui.builder.push_button_builder import PushButtonBuilder
 from src.gui.detail.component import Component
 from src.gui.signals.management import ManagementSignalsSingleton
-from src.gui.detail.settings import BUTTON_DEFAULT_HEIGHT, BUTTON_DEFAULT_WIDTH
+from src.gui.detail.settings import BUTTON_DEFAULT_HEIGHT, BUTTON_DEFAULT_WIDTH, DIMENSION_UNIT_SIZE, LABEL_DEFAULT_SIZE
 from src.gui.detail.styles import get_button_hover_effect
 from src.utils.file_strategy.file_strategy_factory import FileStrategy_en, FileStrategyFactory
 from src.utils.logger import Logger, Level_en
@@ -25,12 +26,15 @@ class CipherManagementComponent(Component):
 
     def initialize_ui(self) -> None:
         SPACING = 10
+        label_builder = (LabelBuilder().set_width(LABEL_DEFAULT_SIZE * 1.5)
+                                       .set_height(DIMENSION_UNIT_SIZE))
         push_button_builder = PushButtonBuilder().set_height(BUTTON_DEFAULT_HEIGHT)
 
         management_box = QtWidgets.QHBoxLayout(self)
         management_box.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
         management_box.setSpacing(SPACING)
 
+        management_box.addWidget(label_builder.set_text("Encrypted (Cipher): ").build())
         upload_btn: QtWidgets.QPushButton = (push_button_builder.set_text("Upload")
                                                                 .set_width(BUTTON_DEFAULT_WIDTH)
                                                                 .set_style(f"QPushButton {"{ border: none; background: #F44336; border-radius: 10px; padding: 8px; }"}{get_button_hover_effect("#F44336")}").build())
