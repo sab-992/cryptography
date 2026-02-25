8a4835fbb343456f9112de6d13360ff30f5c4060
sab-992 <sabri.abdi@polymtl.ca>
2026-02-25 17:33:20 -0500

    removed need for file strategy combo box and added upload + save

--- src/gui/components/management.py
+++ src/gui/components/management.py
@@ -1,21 +1,19 @@
+from pathlib import Path
 from PySide6 import QtCore, QtWidgets
 from PySide6.QtCore import Slot
 from src.cipher.detail.type import CipherDict
-from src.gui.builder.combo_box_builder import ComboBoxBuilder
 from src.gui.builder.push_button_builder import PushButtonBuilder
 from src.gui.detail.component import Component
 from src.gui.signals.management import ManagementSignalsSingleton
-from src.gui.detail.settings import BUTTON_DEFAULT_HEIGHT, BUTTON_DEFAULT_WIDTH, COMBO_BOX_DEFAULT_WIDTH, DIMENSION_UNIT_SIZE
+from src.gui.detail.settings import BUTTON_DEFAULT_HEIGHT, BUTTON_DEFAULT_WIDTH
 from src.gui.detail.styles import get_button_hover_effect
-from src.utils.file_strategy.file_strategy_factory import FileStrategy, FileStrategy_en, FileStrategyFactory
+from src.utils.file_strategy.file_strategy_factory import FileStrategy_en, FileStrategyFactory
 from src.utils.logger import Logger, Level_en
 
 
 class ManagementComponent(Component):
     def __init__(self):
         self.management_signals_s: ManagementSignalsSingleton = ManagementSignalsSingleton()
-        self.file_strategy_combo_box = None
-        self.current_file_strategy: FileStrategy = None
 
         super().__init__(row=1, col=2, alignment=QtCore.Qt.AlignmentFlag.AlignRight)
 
@@ -27,19 +25,11 @@ class ManagementComponent(Component):
 
     def initialize_ui(self) -> None:
         SPACING = 10
-
-        combo_box_builder = (ComboBoxBuilder().set_width(COMBO_BOX_DEFAULT_WIDTH)
-                                              .set_height(DIMENSION_UNIT_SIZE // 1.5))
         push_button_builder = PushButtonBuilder().set_height(BUTTON_DEFAULT_HEIGHT)
 
         management_box = QtWidgets.QHBoxLayout(self)
         management_box.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
         management_box.setSpacing(SPACING)
-
-        self.file_strategy_combo_box: QtWidgets.QComboBox = combo_box_builder.set_values(self.get_file_strategies()).build()
-        self.current_file_strategy = FileStrategyFactory.get(self.file_strategy_combo_box.currentText())
-        self.file_strategy_combo_box.currentIndexChanged.connect(self.on_file_strategy_changed)
-        management_box.addWidget(self.file_strategy_combo_box)
 
         upload_btn: QtWidgets.QPushButton = (push_button_builder.set_text("Upload")
                                                                 .set_width(BUTTON_DEFAULT_WIDTH)
@@ -54,19 +44,14 @@ class ManagementComponent(Component):
         management_box.addWidget(upload_btn)
         management_box.addWidget(save_btn)
 
-    @Slot(int)
-    def on_file_strategy_changed(self, index: int) -> None:
-        if not self.file_strategy_combo_box:
-            raise Exception(Logger.log(message=f"File strategy combo box is not defined", level=Level_en.ERROR))
-
-        self.current_file_strategy = FileStrategyFactory.get(self.file_strategy_combo_box.itemText(index))
-
     @Slot(CipherDict)
     def on_payload_prepared(self, payload: CipherDict) -> None:
-        # TODO: Allow file browsing to get file path
-        file_path: str = ""
-        # self.current_file_strategy.save(file_path, payload)
-        print(f"Payload to save received: {payload} !")
+        file_path_info = QtWidgets.QFileDialog.getSaveFileName(self, "Save File",str(Path(__file__).parent.resolve()), f"Files: (*{" *".join(FileStrategyFactory.supported())});;All Files (*)")
+
+        if not file_path_info or self.path_is_empty(file_path_info[0]):
+            return
+        
+        FileStrategyFactory.get(Path(file_path_info[0]).suffix).save(file_path_info[0], payload)
 
     @Slot()
     def on_save_btn_clicked(self) -> None:
@@ -74,7 +59,18 @@ class ManagementComponent(Component):
 
     @Slot()
     def on_upload_btn_clicked(self) -> None:
-        # TODO: Allow file browsing to get file path
-        file_path = ""
-        # self.management_signals_s.emit("text_overwrite_requested", self.current_file_strategy.read(file_path))
-        print("Read payload to send !")
+        file_path_info = QtWidgets.QFileDialog.getOpenFileName(self, "Open File",str(Path(__file__).parent.resolve()), f"Files: (*{" *".join(FileStrategyFactory.supported())})")
+
+        if not file_path_info or self.path_is_empty(file_path_info[0]):
+            return
+        
+        file = FileStrategyFactory.get(Path(file_path_info[0]).suffix).read(file_path_info[0])
+        if not file:
+            Logger.log(message="File is ill-formed. Make sure it is written correctly", level=Level_en.WARNING, to_std_out=True)
+        elif len(file) <= 0:
+            Logger.log(message="File is empty", level=Level_en.WARNING, to_std_out=True)
+        else:
+            self.management_signals_s.emit("text_overwrite_requested", file)
+
+    def path_is_empty(self, path: str):
+        return not path or len(path) <= 0
