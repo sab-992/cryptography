ab41f7e0d7ee3ac4edf051d7813ea5a5d02d55b4
sab-992 <sabri.abdi@polymtl.ca>
2026-02-25 17:31:54 -0500

    updated factory to also accept extensions

--- src/utils/file_strategy/file_strategy_factory.py
+++ src/utils/file_strategy/file_strategy_factory.py
@@ -9,16 +9,31 @@ class FileStrategy_en(Enum):
     JSON = JsonFS
 
 class FileStrategyFactory:
-    factory: dict[str, Type[FileStrategy]] = { file_strategy.value.as_string(): file_strategy.value for file_strategy in FileStrategy_en}
+    supported_strategies: dict[str, Type[FileStrategy]] = {  f".{str.lower(file_strategy.value.as_string())}": file_strategy.value for file_strategy in FileStrategy_en }
 
     @classmethod
     def get(cls, strategy: Any) -> FileStrategy:
+        # Get using Enum
         if isinstance(strategy, FileStrategy_en):
             return strategy.value()
 
-        obj_type = cls.factory.get(strategy, None)
+        obj_type = None
+        # Get using extension ex: .json
+        if strategy in cls.supported_strategies:
+            obj_type = cls.supported_strategies.get(strategy)
+        else:
+            # Get using displayed name (FileStrategy.as_string()) ex: JSON
+            extension = f".{str.lower(strategy)}"
+
+            if extension in cls.supported_strategies:
+                obj_type = cls.supported_strategies.get(extension)
 
         if not obj_type:
             raise Exception(Logger.log(message=f"File strategy {strategy} is not handled", level=Level_en.ERROR))
         
         return obj_type()
+
+    @classmethod
+    def supported(cls):
+        return cls.supported_strategies.keys()
+            
