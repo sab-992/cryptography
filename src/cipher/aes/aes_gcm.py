343c2cebf96198ab577075b901c377500b5f6b5c
sab-992 <sabri.abdi@polymtl.ca>
2026-02-27 02:01:37 -0500

    stopped using AES wrapper

--- src/cipher/aes/aes_gcm.py
+++ src/cipher/aes/aes_gcm.py
@@ -1,11 +1,15 @@
 import secrets
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM as AESGCM_c
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers.algorithms import AES256
+from cryptography.hazmat.primitives.ciphers.modes import GCM
 from src.cipher.detail.algorithm import Algorithm
 from src.cipher.detail.utils import CipherDict
 from src.utils.file_strategy.file_strategy_factory import FileStrategy, FileStrategy_en, FileStrategyFactory
 
 
-NONCE_SIZE: int = 12
+IV_SIZE: int = 12
+ENCODING: str = "utf-8"
+TAG_SIZE: int = 16
 
 class AESGCM(Algorithm):
     name: str = "AES"
@@ -15,9 +19,19 @@ class AESGCM(Algorithm):
         super().__init__(strategy)
 
     def get_plain(self, password: str, cipher_dict: CipherDict) -> str:
-        key_dict = self.get_key(password, bytes.fromhex(cipher_dict["salt"]))
-        return AESGCM_c(key_dict["key"]).decrypt(nonce=bytes.fromhex(cipher_dict["nonce"]), data=bytes.fromhex(cipher_dict["cipher"]), associated_data=None).decode("utf-8")
+        salt = bytes.fromhex(cipher_dict["salt"])
+        key_dict = self.get_key(password, salt)
+        cipher = bytes.fromhex(cipher_dict["cipher"])[:-TAG_SIZE]
+        nonce = bytes.fromhex(cipher_dict["nonce"])
+        tag = bytes.fromhex(cipher_dict["cipher"])[-TAG_SIZE:]
+
+        decryptor = Cipher(AES256(key_dict["key"]), GCM(initialization_vector=nonce, tag=tag)).decryptor()
+        decrypted_bytes = decryptor.update(cipher) + decryptor.finalize()
+        return decrypted_bytes.decode(ENCODING)
 
     def get_cipher_dict(self, password: str, decrypted: str) -> CipherDict:
-        key_dict, nonce = self.get_key(password), secrets.token_bytes(NONCE_SIZE)
-        return { "cipher": AESGCM_c(key_dict["key"]).encrypt(nonce, decrypted.encode("utf-8"), None).hex(), "nonce": nonce.hex(), "salt": key_dict["salt"].hex(), "cipher_algorithm_used": str(self) }
+        key_dict = self.get_key(password)
+        nonce = secrets.token_bytes(IV_SIZE)
+        encryptor = Cipher(AES256(key_dict["key"]), GCM(initialization_vector=nonce)).encryptor()
+        encrypted_bytes = encryptor.update(decrypted.encode(ENCODING)) + encryptor.finalize()
+        return { "cipher": (encrypted_bytes + encryptor.tag).hex(), "nonce": nonce.hex(), "salt": key_dict["salt"].hex(), "cipher_algorithm_used": str(self) }
